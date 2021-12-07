93947d42c10c09c263ce9f7f6769baaedab726d5
IrvinA <arevalosirvin@gmail.com>
2021-12-07 09:50:21 -0800

    cars-model complete

--- api/server.js
+++ api/server.js
@@ -12,8 +12,8 @@ server.use('*', (req, res, next) => {
   next({ status: 404, message: 'not found' });
 });
 
+// eslint-disable-next-line no-unused-vars
 server.use((err, req, res, next) => {
-  // eslint-disable-line
   res.status(err.status || 500).json({
     message: err.message,
   });
