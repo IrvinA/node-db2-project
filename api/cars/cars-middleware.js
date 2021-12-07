c124fc836ef05ff18ef53b86896e89ca84030985
IrvinA <arevalosirvin@gmail.com>
2021-12-07 10:00:09 -0800

    3rd middleware complete

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -44,7 +44,14 @@ const checkCarPayload = (req, res, next) => {
 };
 
 const checkVinNumberValid = (req, res, next) => {
-  // DO YOUR MAGIC
+  if (vin.validate(req.body.vin)) {
+    next();
+  } else {
+    next({
+      status: 400,
+      message: `vin ${req.body.vin} is invalid`,
+    });
+  }
 };
 
 const checkVinNumberUnique = (req, res, next) => {
