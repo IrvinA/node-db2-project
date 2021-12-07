7bd10a727e09d99e0868074adef3de9c32bb317d
IrvinA <arevalosirvin@gmail.com>
2021-12-07 10:06:28 -0800

    get endpoint complete

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -11,4 +11,13 @@ const {
 
 const router = express.Router();
 
+router.get('/', async (req, res, next) => {
+  try {
+    const cars = await Cars.getAll();
+    res.json(cars);
+  } catch (err) {
+    next(err);
+  }
+});
+
 module.exports = router;
