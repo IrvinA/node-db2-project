a206549f781ccb6d7e4bec5be9b890ed0297ee69
IrvinA <arevalosirvin@gmail.com>
2021-12-07 10:11:04 -0800

    router.js complete

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -20,8 +20,24 @@ router.get('/', async (req, res, next) => {
   }
 });
 
+// eslint-disable-next-line no-unused-vars
 router.get('/:id', checkCarId, async (req, res, next) => {
   res.json(req.car);
 });
+
+router.post(
+  '/',
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
+  async (req, res, next) => {
+    try {
+      const car = await Cars.create(req.body);
+      res.json(car);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
 
 module.exports = router;
