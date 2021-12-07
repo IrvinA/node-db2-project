a1844048d627a3891e47c7fd660dab0af7e871af
IrvinA <arevalosirvin@gmail.com>
2021-12-07 10:04:44 -0800

    cars-router intial setup complete

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,14 @@
-// DO YOUR MAGIC
+const express = require('express');
+
+const Cars = require('./cars-model');
+
+const {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberUnique,
+  checkVinNumberValid,
+} = require('./cars-middleware');
+
+const router = express.Router();
+
+module.exports = router;
